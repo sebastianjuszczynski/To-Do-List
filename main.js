1c32da51b45483442a25d8b7ce416056f2e3b506
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-03 21:43:26 +0000

    added functions onFormSubmit and addNewTask

--- main.js
+++ main.js
@@ -9,6 +9,13 @@
             done: true,
         }
     ];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    }
     const render = () => {
         let htmlString = "";
 
@@ -24,8 +31,24 @@
         document.querySelector(".js-tasksList").innerHTML = htmlString;
     };
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskElement = document.querySelector(".js-input");
+        const newTaskContent = newTaskElement.value.trim();
+        if (newTaskContent === "") {
+            return;
+        } else  {
+            addNewTask(newTaskContent);
+            newTaskElement.value = "";
+        }
+        newTaskElement.focus();
+    };
+
     const init = () => {
         render();
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
     init();
 }
