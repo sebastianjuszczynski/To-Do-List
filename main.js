27c3667ade5dd4027a2e90340ca50a79f869ecb3
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-04 18:15:35 +0000

    function toggleTaskDone refactored

--- main.js
+++ main.js
@@ -17,7 +17,14 @@
         render();
     }
     const toggleTaskDone = (index) => {
-        tasks[index].done = !tasks[index].done;
+        tasks = [
+            ...tasks.slice(0, index),
+            {
+                ...tasks[index],
+                done: !tasks[index].done,
+            },
+            ...tasks.slice(index + 1),
+        ];
         render();
     }
 
